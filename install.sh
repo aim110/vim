e4359c5e7742c0a1645f806ab0b1ce5c5d5bb0f3
qqq <qqq@leprosus-dev.yandex.ru>
2013-09-16 14:39:35 +0400

    pathogen installation wombat theme installation

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 set -x
 
+pip install jedi
+
+if [[ ! -e ~/.vim/bundle ]]; then mkdir -p ~/.vim/bundle; fi
+if [[ ! -e ~/.vim/autoload ]]; then mkdir -p ~/.vim/autoload; fi
+if [[ ! -e ~/.vim/colors ]]; then mkdir -p ~/.vim/colors; fi
+
+curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
 cd ~/.vim/bundle
 git clone git://github.com/Lokaltog/vim-powerline.git
 
@@ -7,7 +15,6 @@ mkdir -p ~/.vim/ftplugin
 wget -O ~/.vim/ftplugin/python_editing.vim http://www.vim.org/scripts/download_script.php?src_id=5492
 http://www.vim.org/scripts/script.php?script_id=1494
 
-pip install jedi
 cd ~/.vim/bundle
 git clone git://github.com/davidhalter/jedi-vim.git
 cd jedi-vim
@@ -15,3 +22,6 @@ git submodule update --init
 
 cd ~/.vim/bundle
 git clone https://github.com/kien/ctrlp.vim.git
+
+cd ~/.vim/colors
+wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
