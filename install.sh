b1e9616df07e7a3a07c0e434c3cde612d3325b60
IVan <i.michailyukov@gmail.com>
2015-04-27 17:52:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
+set -e
 set -x
 
+source .env/bin/activate
 pip install jedi
 
 if [[ ! -e ~/.vim/bundle ]]; then mkdir -p ~/.vim/bundle; fi
 if [[ ! -e ~/.vim/autoload ]]; then mkdir -p ~/.vim/autoload; fi
 if [[ ! -e ~/.vim/colors ]]; then mkdir -p ~/.vim/colors; fi
 
-curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+curl -q -O ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim > ~/.vim/autoload/pathogen.vim
 
 cd ~/.vim/bundle
 git clone git://github.com/Lokaltog/vim-powerline.git
@@ -25,3 +27,7 @@ git clone https://github.com/kien/ctrlp.vim.git
 
 cd ~/.vim/colors
 wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
+
+cd ~/.vim/bundle
+git clone https://github.com/vim-scripts/pep8
+pip install pep8
